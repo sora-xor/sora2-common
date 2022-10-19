fb0f3b04ca003c65c768a5d85beae064f8645bc0
N1ghtStorm <bushuevdv@mail.ru>
2022-10-20 01:06:42 +0200

    add bitvec

--- pallets/sora2-trustless-bridge/common/src/bitfield.rs
+++ pallets/sora2-trustless-bridge/common/src/bitfield.rs
@@ -1,6 +1,52 @@
+use core::{ops::Deref, usize};
+
 use scale_info::prelude::vec::Vec;
+use bitvec::{prelude::*, ptr::BitSpanError};
 
 pub const SIZE: u128 = core::mem::size_of::<u128>() as u128;
+pub struct BitField(BitVec<u8, Msb0>);
+
+impl BitField {
+    // Constuctors:
+
+    pub fn with_capacity(bit: bool, len: usize) -> Self {
+        Self(BitVec::repeat(bit, len))
+    }
+
+    pub fn try_from_slice(slice: &[u8]) -> Result<Self, BitSpanError<u8>> {
+        Ok(Self(BitVec::try_from_slice(slice)?))
+    }
+
+    // Util
+
+    pub fn count_set_bits(&self) -> u128 {
+        self.0.count_ones() as u128
+    }
+
+    pub fn to_bits(self) -> Vec<u8> {
+        self.0.into_vec()
+    }
+
+    pub fn set(&mut self, index: usize) {
+        self.0.set(index, true)
+    }
+
+    pub fn clear(&mut self, index: usize) {
+        self.0.set(index, false)
+    }
+
+    pub fn is_set(&self, index: usize) -> bool {
+        self.0[index] == true
+    }
+}
+
+impl Deref for BitField {
+    type Target = BitVec<u8, Msb0>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 pub fn create_bitfield(bits_to_set: Vec<u128>, length: u128) -> Vec<u128> {
     let array_length = (length + 255) / 256;
@@ -35,6 +81,18 @@ pub fn count_set_bits(_self_val: Vec<u128>) -> u128 {
     let count = 0;
     count
 }
+
+pub fn count_set_bits_bv(bitvec: BitField) -> u128 {
+    bitvec.count_ones() as u128
+}
+
+// pub fn create_bitfield_bv(bits_to_set: Vec<u128>, length: u128) -> BitField {
+//     // let mut bv = bitvec![u8, Msb0;];
+//     // let mut bv = BitVec::repeat(false, 10);
+//     // bv.push(false);
+//     // bv.push(true);
+//     // bv
+// }
 
 #[cfg(test)]
 mod test {
