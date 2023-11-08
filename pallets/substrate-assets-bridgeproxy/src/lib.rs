ab3909cc29549bcf8e9dc9c9f64bd2f35a599138
Dmitry <bushuevdv@mail.ru>
2023-11-08 17:44:17 -0300

    add tech acc as constant

--- pallets/substrate-assets-bridgeproxy/src/lib.rs
+++ pallets/substrate-assets-bridgeproxy/src/lib.rs
@@ -15,44 +15,21 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![warn(missing_docs)]
-
-//! A BEEFY+MMR pallet combo.
-//!
-//! While both BEEFY and Merkle Mountain Range (MMR) can be used separately,
-//! these tools were designed to work together in unison.
-//!
-//! The pallet provides a standardized MMR Leaf format that is can be used
-//! to bridge BEEFY+MMR-based networks (both standalone and polkadot-like).
-//!
-//! The MMR leaf contains:
-//! 1. Block number and parent block hash.
-//! 2. Merkle Tree Root Hash of next BEEFY validator set.
-//! 3. Merkle Tree Root Hash of current parachain heads state.
-//!
-//! and thanks to versioning can be easily updated in the future.
 
 pub use pallet::*;
 use sp_core::H256;
-
-/// Subject for randomness.
-// pub const RANDOMNESS_SUBJECT: &[u8] = b"beefy-leaf-extra";
-
-/// A type that is able to return current list of parachain heads that end up in the MMR leaf.
 
 #[frame_support::pallet]
 pub mod pallet {
     #![allow(missing_docs)]
     use crate::AssetIdGenerator;
-    use bridge_types::types::RawAssetInfo;
     use bridge_types::GenericNetworkId;
     use frame_support::pallet_prelude::{ValueQuery, *};
     use frame_support::traits::fungibles::{
         metadata::Mutate as MetadataMutate, Create, Inspect, InspectMetadata, Transfer, Mutate,
     };
-    use frame_system::{pallet_prelude::*, Origin};
+    use frame_system::pallet_prelude::*;
     use sp_core::H256;
-    use sp_runtime::traits::StaticLookup;
     use sp_std::prelude::*;
     use sp_io::hashing::blake2_256;
 
@@ -76,7 +53,7 @@ pub mod pallet {
         /// The overarching event type.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-        // type Randomness: Randomness<RandomnessOutputOf<Self>, Self::BlockNumber>;
+        #[pallet::constant]
         type TechAcc: Get<Self::AccountId>;
 
         type AssetIdGenerator: AssetIdGenerator<Self::AssetId>;
@@ -112,13 +89,9 @@ pub mod pallet {
             name: <Self as bridge_types::traits::BridgeAssetRegistry<T::AccountId, T::AssetId>>::AssetName,
             symbol: <Self as bridge_types::traits::BridgeAssetRegistry<T::AccountId, T::AssetId>>::AssetSymbol,
         ) -> Result<T::AssetId, DispatchError> {
-            // Err(DispatchError::Other("NOT AVAILIBLE"))
             let nonce = Self::asset_nonce();
             AssetNonce::<T>::set(nonce + 1);
-            // let ta = T::TechAcc::get();
-            // let ta = account_id();
-            
-            let ta = Self::tech_acc().unwrap();
+            let ta = T::TechAcc::get();
             let mut i = 0;
             let iter = 4;
             while i <= iter {
@@ -126,25 +99,16 @@ pub mod pallet {
                     let mut vector = name.clone();
                     vector.extend_from_slice(&symbol);
                     vector.extend_from_slice(&(nonce + i).encode());
-                    // let hash = sp_core::hashing::blake2_256(&vector);
                     let hash = blake2_256(&vector);
                     H256::from_slice(&hash)
                 };
                 let asset_id = T::AssetIdGenerator::generate_asset_id(hash);
-                // let asset_id = (10000 + nonce).into();
-
                 let res = <pallet_assets::Pallet<T> as Create<T::AccountId>>::create(
                     asset_id,
                     ta.clone(),
                     true,
                     T::MinBalance::get(),
                 );
-                // let res = <pallet_assets::Pallet<T> as Create<T::AccountId>>::create(
-                //     (10000 + nonce).into(),
-                //     ta.clone(),
-                //     true,
-                //     T::MinBalance::get(),
-                // );
                 if res.is_ok() {
                     <pallet_assets::Pallet<T> as MetadataMutate<T::AccountId>>::set(
                         asset_id, &ta, name, symbol, 18,
@@ -160,7 +124,6 @@ pub mod pallet {
         }
 
         fn manage_asset(_: GenericNetworkId, _: T::AssetId) -> Result<(), DispatchError> {
-            // Err(DispatchError::Other("NOT AVAILIBLE"))
             Ok(())
         }
 
@@ -191,10 +154,7 @@ pub mod pallet {
             asset_id: &Self::AssetId,
             amount: &Self::Balance,
         ) -> DispatchResult {
-            // let ta = T::TechAcc::get();
-            let ta = Self::tech_acc().unwrap();
-            // let ta = Self::account_id();
-
+            let ta = T::TechAcc::get();
             match asset_kind {
                 bridge_types::types::AssetKind::Thischain => {
                     <pallet_assets::Pallet<T> as Transfer<T::AccountId>>::transfer(
@@ -224,9 +184,7 @@ pub mod pallet {
             asset_id: &Self::AssetId,
             amount: &Self::Balance,
         ) -> DispatchResult {
-            // let ta = T::TechAcc::get();
-            // let ta = Self::account_id();
-            let ta = Self::tech_acc().unwrap();
+            let ta = T::TechAcc::get();
             match asset_kind {
                 bridge_types::types::AssetKind::Thischain => {
                     <pallet_assets::Pallet<T> as Transfer<T::AccountId>>::transfer(
@@ -249,19 +207,6 @@ pub mod pallet {
             Ok(())
         }
     }
-
-    // impl<T: Config> Pallet<T> { 
-    //     pub fn account_id() -> T::AccountId {
-    //         use frame_support::PalletId;
-    //         use sp_runtime::traits::AccountIdConversion;
-        
-    //         const PALLET_ID: PalletId = PalletId(*b"brdgprxy");
-    //         // Convert the PalletId into an AccountId using the AccountIdConversion trait
-    //         PALLET_ID.into_account()
-    //     }
-    // }
-
-
 }
 
 pub trait AssetIdGenerator<T> {
