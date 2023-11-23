5506f7ec023899309b2d7050625a74d54aee6244
C4tWithShell <shelkovnikov@soramitsu.co.jp>
2023-11-23 09:40:32 +0300

    Add coverage script

---
+++ housekeeping/coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export RUSTFLAGS="-Cinstrument-coverage"
+export SKIP_WASM_BUILD=1
+export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
+
+cargo test
+
+grcov . --binary-path ./target/debug -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug"
+find . -type f -name '*.profraw' -delete
