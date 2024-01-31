ef025a4ab3ca4ab1f876724766d88debfd518dd3
Vladimir Shelkovnikov <99356504+C4tWithShell@users.noreply.github.com>
2024-01-31 11:38:55 +0300

    Fix comments

--- housekeeping/clippy.sh
+++ housekeeping/clippy.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$pr" = true ] && [ "$prBranch" != "master" ]; then
     printf "👷‍♂️ starting clippy \n"
     SKIP_WASM_BUILD=1 cargo clippy
-    SKIP_WASM_BUILD=1 cargo clippy --features runtime-benchmarks  --message-format=json > clippy_report.json
+    SKIP_WASM_BUILD=1 cargo clippy --features runtime-benchmarks  --message-format=json -- -D warnings > clippy_report.json
 else
     printf "👷‍♂️ starting a regular clippy \n"
     cargo clippy --message-format=json -- -D warnings > clippy_report.json || exit 0
