2f5652869c708acba9ec6d2882fb8e5c37c53323
Vladimir Stepanenko <vovac12@gmail.com>
2024-02-01 05:34:32 +0300

    Update outbound channel interval (#73)

---
+++ pallets/substrate-channel/src/outbound/mock.rs
@@ -0,0 +1,204 @@
+use super::*;
+use bridge_types::GenericNetworkId;
+use codec::{Decode, Encode, MaxEncodedLen};
+use currencies::BasicCurrencyAdapter;
+
+use bridge_types::traits::TimepointProvider;
+use frame_support::traits::{Everything, GenesisBuild};
+use frame_support::{parameter_types, Deserialize, Serialize};
+use scale_info::TypeInfo;
+use sp_core::H256;
+use sp_keyring::AccountKeyring as Keyring;
+use sp_runtime::testing::Header;
+use sp_runtime::traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify};
+use sp_runtime::{AccountId32, MultiSignature};
+use sp_std::convert::From;
+use traits::parameter_type_with_key;
+
+use crate::outbound as bridge_outbound_channel;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+pub const BASE_NETWORK_ID: SubNetworkId = SubNetworkId::Mainnet;
+
+frame_support::construct_runtime!(
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Pallet, Call, Storage, Event<T>},
+        Timestamp: pallet_timestamp::{Pallet, Call, Storage},
+        Tokens: tokens::{Pallet, Call, Config<T>, Storage, Event<T>},
+        Currencies: currencies::{Pallet, Call, Storage},
+        Balances: pallet_balances::{Pallet, Call, Storage, Event<T>},
+        BridgeOutboundChannel: bridge_outbound_channel::{Pallet, Config<T>, Storage, Event<T>},
+    }
+);
+
+pub type Signature = MultiSignature;
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+#[derive(
+    Encode,
+    Decode,
+    PartialEq,
+    Eq,
+    Debug,
+    Clone,
+    Copy,
+    MaxEncodedLen,
+    TypeInfo,
+    PartialOrd,
+    Ord,
+    Serialize,
+    Deserialize,
+)]
+pub enum AssetId {
+    XOR,
+    ETH,
+    DAI,
+}
+
+pub type Balance = u128;
+pub type Amount = i128;
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+}
+
+impl frame_system::Config for Test {
+    type BaseCallFilter = Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = BlockHashCount;
+    type DbWeight = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<65536>;
+}
+
+parameter_types! {
+    pub const ExistentialDeposit: u128 = 0;
+}
+
+parameter_type_with_key! {
+    pub ExistentialDeposits: |_currency_id: AssetId| -> Balance {
+        0
+    };
+}
+
+impl pallet_balances::Config for Test {
+    type Balance = Balance;
+    type RuntimeEvent = RuntimeEvent;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type WeightInfo = ();
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = ();
+}
+
+impl tokens::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type Balance = Balance;
+    type Amount = Amount;
+    type CurrencyId = AssetId;
+    type WeightInfo = ();
+    type ExistentialDeposits = ExistentialDeposits;
+    type CurrencyHooks = ();
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = ();
+    type DustRemovalWhitelist = Everything;
+}
+
+impl currencies::Config for Test {
+    type MultiCurrency = Tokens;
+    type NativeCurrency = BasicCurrencyAdapter<Test, Balances, Amount, u64>;
+    type GetNativeCurrencyId = GetBaseAssetId;
+    type WeightInfo = ();
+}
+parameter_types! {
+    pub const GetBaseAssetId: AssetId = AssetId::XOR;
+    pub GetTeamReservesAccountId: AccountId = AccountId32::from([0; 32]);
+    pub GetFeeAccountId: AccountId = AccountId32::from([1; 32]);
+    pub GetTreasuryAccountId: AccountId = AccountId32::from([2; 32]);
+}
+
+parameter_types! {
+    pub const MaxMessagePayloadSize: u32 = 128;
+    pub const MaxMessagesPerCommit: u32 = 5;
+    pub const ThisNetworkId: GenericNetworkId = GenericNetworkId::Sub(SubNetworkId::Mainnet);
+}
+
+pub struct GenericTimepointProvider;
+
+impl TimepointProvider for GenericTimepointProvider {
+    fn get_timepoint() -> bridge_types::GenericTimepoint {
+        bridge_types::GenericTimepoint::Sora(System::block_number() as u32)
+    }
+}
+
+impl bridge_outbound_channel::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type MaxMessagePayloadSize = MaxMessagePayloadSize;
+    type MaxMessagesPerCommit = MaxMessagesPerCommit;
+    type MessageStatusNotifier = ();
+    type AuxiliaryDigestHandler = ();
+    type AssetId = ();
+    type Balance = u128;
+    type WeightInfo = ();
+    type TimepointProvider = GenericTimepointProvider;
+    type ThisNetworkId = ThisNetworkId;
+}
+
+impl pallet_timestamp::Config for Test {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = ();
+    type WeightInfo = ();
+}
+
+pub fn new_tester() -> sp_io::TestExternalities {
+    let mut storage = frame_system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap();
+
+    let config: bridge_outbound_channel::GenesisConfig<Test> =
+        bridge_outbound_channel::GenesisConfig {
+            interval: 10u32.into(),
+        };
+    config.assimilate_storage(&mut storage).unwrap();
+
+    let bob: AccountId = Keyring::Bob.into();
+
+    pallet_balances::GenesisConfig::<Test> {
+        balances: vec![(bob, 1u32.into())],
+    }
+    .assimilate_storage(&mut storage)
+    .unwrap();
+
+    let mut ext: sp_io::TestExternalities = storage.into();
+
+    ext.execute_with(|| System::set_block_number(1));
+    ext
+}
